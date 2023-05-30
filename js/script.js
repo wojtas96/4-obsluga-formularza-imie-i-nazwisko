1ea6629a055a06d3c31076117d939f0b005f0080
Klaudia <koliber96k@gmail.com>
2023-05-31 00:14:02 +0200

    4-obsluga-formularza-imie-i-nazwisko

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("4-obsluga-formularza-imie-i-nazwisko");
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fName"]');
+    let lastName = document.querySelector('[name="lname"]');
+
+    
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+
+    
+    // console.log(firstName);
+    // console.log(lastName);
+
+    // console.log(`Submit FORM`);
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submitForm);
+
+// console.log(form);
